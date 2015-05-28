20afb1809ecccc9f5611600700c2dc54486a7225
CenkSl <cenksaldirim@gmail.com>
2015-05-28 21:49:23 +0300

    Client

---
+++ Client/Client/Class/MiniSocket.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class MiniSocket
+    {
+        public MiniSocket(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType)
+        {
+            connection = new Socket(addressFamily, socketType, protocolType);
+            isConnected = false;
+        }
+
+        ~MiniSocket()
+        {
+            connection.Close();
+        }
+
+
+        // Member Functions
+
+        public void connectToHost(string ip, string port)
+        {
+            // Generates server connection using given ip address and port
+            connection.Connect(IPAddress.Parse(ip), Int32.Parse(port));
+            stream = new NetworkStream(connection);
+            reader = new BinaryReader(stream);
+            writer = new BinaryWriter(stream);
+            isConnected = true;
+        }
+
+        public bool getConnectionProperty()
+        {
+            return isConnected;
+        }
+
+        public void write(string message)
+        {
+            writer.Write(message); // Send information to server
+        }
+
+        public string read()
+        {
+            return reader.ReadString(); // Recieve message from server
+        }
+
+        public void StreamFlush()
+        {
+            stream.Flush();
+        }
+
+        // Private Data Members
+
+        private Socket connection;
+        private Boolean isConnected;
+
+        // Protected Data Members & Properties
+
+        protected NetworkStream stream { get; set; }
+        protected BinaryReader reader { get; set; }
+        protected BinaryWriter writer { get; set; }
+    }
+}
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripStatusLabel1.Text = "Ready";
+        }
+        MiniSocket connection = new MiniSocket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!connection.getConnectionProperty()) // 
+            {
+                
+                connection.connectToHost(textBox1.Text, textBox2.Text);
+                button2.Enabled = true;
+                toolStripStatusLabel1.Text = "Connected";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (connection.getConnectionProperty())
+            {
+                try
+                {
+                    connection.write("toggle");
+                    string receivedMessage = connection.read();
+                    if (receivedMessage == "ledon")
+                    {
+                        label3.ForeColor = Color.Lime;
+                        label3.Text = "Led On";
+                        button2.Text = "Led Off";
+                    }
+                    else if (receivedMessage == "ledoff")
+                    {
+                        label3.ForeColor = Color.Gray;
+                        label3.Text = "Led Off";
+                        button2.Text = "Led On";
+                    }
+                    connection.StreamFlush();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex);
+                }
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Not connected";
+            }
+        }
+    }
+}
